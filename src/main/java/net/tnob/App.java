172e0956e3c3019c413d24062d723ed7eb56c7d9
Tahmid-un Nabi <sinetob@gmail.com>
2014-10-11 13:23:44 -0700

    initial commit

---
+++ src/main/java/net/tnob/App.java
@@ -0,0 +1,14 @@
+package net.tnob;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int answer() {
+        return 42;
+    }
+}
